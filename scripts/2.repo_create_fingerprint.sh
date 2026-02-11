c4b4116a13958d51025f78b561c7823d5ab728de
Александр Мишин <mishin@mh.net.ru>
2026-02-11 23:45:56 +0400

    Update

--- scripts/2.repo_create_fingerprint.sh
+++ scripts/2.repo_create_fingerprint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 REPO=/gammix/repo
-ARCH_LIST='13:armv7 13:aarch64 14:armv7 14:aarch64'
+ARCH_LIST='13:armv7 13:aarch64 14:armv7 14:aarch64 15:armv7 15:aarch64'
 
 for ARCH in ${ARCH_LIST}; do
 /usr/sbin/pkg repo ${REPO}/FreeBSD:${ARCH}/latest signing_command: ./repo_sign.sh
